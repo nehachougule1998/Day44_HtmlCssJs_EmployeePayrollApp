91ff3c53ee2573b35adac521ecddfee3eef975b7
nehachougule1998 <nchougule734@gmail.com>
2022-04-27 13:08:17 +0530

    Added

--- Js/EmployeePayrollData.js
+++ Js/EmployeePayrollData.js
@@ -1,64 +1,57 @@
-class EmployeePayrollData {
-
-    get id() {
-        return this._id = id;
-    }
-    set id(id) {
-        this._id = id;
-    }
-    get name() {
-        return this._name;
-    }
+class EmployeePayroll {
+    id;
+ 
+    get name() { return this._name; }
     set name(name) {
-        let nameRegex = RegExp('^[A-z]{1}[a-z]{2,}$');
-        if (nameRegex.test(name))
-            this._name = name;
-        else throw 'Name is incorrect';
+       let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+       if (nameRegex.test(name))
+          this._name = name;
+       else throw 'Name is Incorrect!';
     }
-    get profilePic() {
-        return this._profilePic;
-    }
+ 
+    get profilePic() { return this._profilePic; }
     set profilePic(profilePic) {
-        this._profilePic = profilePic;
+       this._profilePic = profilePic;
     }
-    get gender() {
-        return this._gender;
-    }
+ 
+    get gender() { return this._gender; }
     set gender(gender) {
-        this._gender = gender;
+       this._gender = gender;
     }
-    get department() {
-        return this._deparment;
+ 
+    get department() { return this._department; }
+    set department(department) {
+       this._department = department;
     }
-    set department(department){
-        this._deparment=department;
+ 
+    get salary() { return this._salary; }
+    set salary(salary) {
+       this._salary = salary;
     }
-    get salary(){
-        return this._salary;
+ 
+    get note() { return this._note; }
+    set note(note) {
+       this._note = note;
     }
-    set salary(salary){
-        this._salary=salary;
+ 
+    get startDate() { return this._startDate; }
+    set startDate(startDate) {
+       let now = new Date();
+       if (startDate > now) throw 'Start Date is a Future Date!';
+       var diff = Math.abs(now.getTime() - startDate.getTime());
+       if (diff / (1000 * 60 * 60 * 24) > 30)
+          throw 'Start Date is beyond 30 Days!';
+       this._startDate = startDate;
     }
-    get note(){
-        return this._note;
+ 
+    // method
+    toString() {
+       const options = { year: 'numeric', month: 'short', day: 'numeric' };
+       const empDate = !this.startDate ? "undefined" :
+          this.startDate.toLocaleDateString("en-GB", options);
+       return "id=" + this.id + ", name='" + this.name + ", gender='" + this.gender +
+          ", profilePic='" + this.profilePic + ", department=" + this.department +
+          ", salary=" + this.salary + ", startDate=" + empDate + ", note=" + this.note;
     }
-    set note(note){
-        this._note=note;
-    }
-    get startDate(){
-        return this._startDate;
-    }
-    set startDate(startDate){
-        this._startDate=startDate;
-    }
-    
-    toString(){
-        const options={year:'numeric',month:'long',day:'numeric'};
-        const empDate=!this._startDate ? "undefined" :
-        this.startDate.toLocalDateString("en-US",options);
-        return "id=" + this.id + ",name='" +this.name + ", gender='" + this.gender +
-        ",profilePic='" + this.profilePic + ", department=" + this.department +
-        ", salary=" + this.salary + ", startDate=" + empDate + ", note=" + this.note;
-    }
-
-}
+ }
+ 
